e5c85c1cc707d615d5c090bd88d281003ad49d65
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-09-18 17:07:57 -0600

    feat: Se agregó funcionalidad para el bloque
    Dependiendo de la resistencia el bloque seguirá o se destruirá

--- FirstGame/Assets/_Scripts/Bloque.cs
+++ FirstGame/Assets/_Scripts/Bloque.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bloque : MonoBehaviour
 {
+    public int resistencia = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Bloque : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (resistencia <= 1) Destroy(this.gameObject);
     }
 }
